a43268f52b9d064e80566dd958d275809c557322
Fede Lizondo <fedealiza@gmail.com>
2019-07-20 11:55:34 -0700

    add changes in server switch

---
+++ server/serverPosgres.c
@@ -0,0 +1,34 @@
+#include "Servidor/servidor.h"
+#include "Response/request_switch.h"
+#include "Servidor/client.h"
+
+#define DEFAULT_PORT 8082
+
+void *onSuccess(void * arg) {
+  //CONEXION CON OTRO SERVIDOR EN C++ U OTRO LENGUAJE FIREBASE NO ESTA PARA C
+  int MAX_SIZE_SEND = 4096;
+  int MAX_SIZE_RECIVE = 1024;
+	int FD_USER = *((int *)arg);
+
+  char mensaje[MAX_SIZE_RECIVE];
+  char response[MAX_SIZE_SEND];
+
+  if(recv(FD_USER, mensaje, sizeof(char) * MAX_SIZE_SEND, 0) > 0) {
+      printf("%s\n",mensaje);
+      printf("%s\n", "Enviando mensaje ...");
+      send(FD_USER,"{ 1:{'NOMBRE':' POSGRESS','APELLIDO':'POLO POSGRESS'}}",sizeof(response),0);
+  }
+  return NULL;
+}
+
+int main(int argc, char *argv[]){
+  int port = (argc > 2) ? atoi(argv[2]) : DEFAULT_PORT;
+  char *ipAddress = (argc > 1) ? argv[1] : NULL;
+  printf("Inciando servidor en IP: %s Puerto: %d\n", (ipAddress == NULL)?"localHost":ipAddress, port);
+
+  ptr_Servidor servidor = servidor_create(ipAddress, port);
+  servidor->onSuccess = onSuccess;
+  servidor_run(servidor);
+
+  return EXIT_SUCCESS;
+}
--- server/serverSwitch.c
+++ server/serverSwitch.c
@@ -1,7 +1,14 @@
 #include "Servidor/servidor.h"
 #include "Response/request_switch.h"
+#include "Servidor/client.h"
 
 #define DEFAULT_PORT 8080
+#define SERVIDOR_FACTURACION "FACTURACION"
+#define SERVIDOR_RECURSOS "RECURSOS"
+#define SERVIDOR_PERSONAL "PERSONAL"
+
+char *delimitatorPuntoYComa = ";";
+char *delimitatorDosPuntos = ":";
 
 void *onSuccess(void *);
 
@@ -9,7 +16,7 @@ int main(int argc, char *argv[])
 {
 	int port = (argc > 2) ? atoi(argv[2]) : DEFAULT_PORT;
 	char *ipAddress = (argc > 1) ? argv[1] : NULL;
-	printf("Inciando servidor en IP: %s Puerto: %d\n", ipAddress, port);
+	printf("Inciando servidor en IP: %s Puerto: %d\n", (ipAddress == NULL)?"localHost":ipAddress, port);
 
 	ptr_Servidor servidor = servidor_create(ipAddress, port);
 	servidor->onSuccess = onSuccess;
@@ -21,26 +28,49 @@ int main(int argc, char *argv[])
 void *onSuccess(void *arg)
 {
 	int MAX_SIZE_SEND = 1024;
+	int MAX_SIZE_RECIVE = 4096;
+
 	int FD_USER = *((int *)arg);
-	printf("%d\n", FD_USER);
 	char mensaje[MAX_SIZE_SEND];
+	char response[MAX_SIZE_RECIVE];
 	bzero(mensaje, MAX_SIZE_SEND);
 
-	if (recv(FD_USER, mensaje, sizeof(char) * MAX_SIZE_SEND, 0) > 0)
+	if(recv(FD_USER, mensaje, sizeof(char) * MAX_SIZE_SEND, 0) > 0)
 	{
-		ptr_sockerRequest request = socketRequest_create(mensaje);
+		//TODO MOVE TO REQUEST_SWITCH
+		char *serverName = NULL;
+		char *query = NULL;
 
-		if (strcmp(request->servidorName, "FACTURACION") == 0)
+		char *token = strtok(mensaje, delimitatorPuntoYComa);
+		if (token != NULL)
 		{
-			printf("Servidor: %s\nQuery: %s", request->servidorName, request->query);
+				serverName = token;
+				token = strtok(NULL, delimitatorPuntoYComa);
+				if (token != NULL)
+				{
+						query = token;
+				}
 		}
-		else
-		{
-			printf("NO ES UN SERVIDOR");
+
+		strtok(serverName, delimitatorDosPuntos);
+		serverName = strtok(NULL, delimitatorDosPuntos);
+
+		if(serverName == NULL){
+			printf("No existe el %s , no se puede enviar la query \"%s\"\n",serverName, query);
 		}
+		else if( strcmp(serverName,SERVIDOR_FACTURACION) == 0) {
+			//send(FD_USER, "ENV", sizeof(char) *  3, 0);
+			printf("%s\n","EN EL SERVIDOR DE FACTURA");
+		}
+		else if( strcmp(serverName,SERVIDOR_PERSONAL) == 0) {
+			printf("%s\n", "ENVIANDO A PERSONAL");
+		}
+		else if( strcmp(serverName,SERVIDOR_RECURSOS) == 0) {
+			printf("%s\n", "ENVIANDO A RECURSOS");
+		}
+
 	}
-	//write(FD_USER, mensaje, sizeof(*mensaje) );
-	printf("Enviando mensaje...\n\t%s", mensaje);
-	send(FD_USER, mensaje, sizeof(mensaje), 0);
+	// write(FD_USER, mensaje, sizeof(*mensaje) );
+	// send(FD_USER, mensaje, sizeof(mensaje), 0);
 	return NULL;
 };
